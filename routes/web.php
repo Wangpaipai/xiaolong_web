ab2dfb5b4fd547a22d152abbc5be81b72c5987c3
廖秀君 <864969742>
2021-08-20 16:06:44 +0800

    静态文件

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('admin.login.login');
 });
 
 Route::get('/dashboard', function () {
