8cc602441854d541ed0a37bc3f41748f3b3aad2a
王小牌 <864969742@qq.com>
2022-04-10 16:25:51 +0800

    禁止缩放

--- resources/views/wap/common/header.blade.php
+++ resources/views/wap/common/header.blade.php
@@ -9,8 +9,8 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="keywords" content="68伞业,68,伞业,罗马伞,六八,六八伞业,遮阳伞,双流区">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
+{{--    <meta name="viewport" content="width=device-width, initial-scale=1">--}}
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" >
 
     <!-- Style -->
     <link rel="stylesheet" href="{{ asset('static/wap/css/theme.css') }}">
